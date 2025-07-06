c50aa1e602e324267162aee6441da48bc339a270
Aryan Raj <aryanraj24032002@gmail.com>
2025-07-06 18:48:02 +0530

    form state added

--- frontend/src/store/index.jsx
+++ frontend/src/store/index.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import commonSlice from "./features/common/commonSlice.js"
+import templateSlice from "./features/template/templateSlice.js"
 
 export const store = configureStore({
   reducer: {
-    common : commonSlice
+    common : commonSlice,
+    template : templateSlice
   },
 })
