d93f8ef0e8f7e58b26bc7af389b5b26ff4507d44
Aryan Raj <aryanraj24032002@gmail.com>
2025-07-14 15:53:03 +0530

    Create Button added

--- frontend/src/pages/teacher/assesments/index.jsx
+++ frontend/src/pages/teacher/assesments/index.jsx
@@ -4,6 +4,8 @@ import { assesmentColumns } from "./utils";
 import { useHeading } from "../../../hooks";
 import { useNavigate } from "react-router-dom";
 import { useAssesmentsData } from "./hooks";
+import CustomButton from "../../../componets/common/CustomButton";
+import { PlusIcon } from "lucide-react";
 
 const TeacherAssesmentsPage = () => {
   const { setHeading, setSubheading } = useHeading();
@@ -14,8 +16,17 @@ const TeacherAssesmentsPage = () => {
 
   const { rows = [], actions = [] } = useAssesmentsData();
 
+  function handleClick(){
+    navigate("/teacher/assesments/create");
+  }
+
   return (
     <div>
+      <div className="mb-2">
+        <CustomButton onClick={handleClick}>
+          <PlusIcon size={20} /> Create Assesment
+        </CustomButton>
+      </div>
       <CustomTable columns={assesmentColumns} data={rows} actions={actions} />
     </div>
   );
--- frontend/src/pages/teacher/templates/index.jsx
+++ frontend/src/pages/teacher/templates/index.jsx
@@ -3,18 +3,31 @@ import CustomTable from '../../../componets/common/CustomTable'
 import { useTemplatesData } from './hooks'
 import { templateColumns } from './utils'
 import { useHeading } from '../../../hooks'
+import CustomButton from '../../../componets/common/CustomButton'
+import { PlusIcon } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 
 const TeacherTemplates = () => {
 
   const { setHeading, setSubheading } = useHeading();
+  const navigate = useNavigate();
   setHeading("Templates");
   setSubheading("This is the place where you can manage your templates");
   
   const { rows = [], actions = [] } = useTemplatesData();
 
+  function handleClick(){
+    navigate("/teacher/templates/create");
+  }
+
   return (
     <div>
+      <div className="mb-2">
+        <CustomButton onClick={handleClick}>
+          <PlusIcon size={20} /> Create Assesment
+        </CustomButton>
+      </div>
       <CustomTable columns={templateColumns} data={rows} actions={actions}/>
     </div>
   )
